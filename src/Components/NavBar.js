3a13b986390a57139f0422744260683a57ca0c7a
Samuel <kungusamuel64@gmail.com>
2022-02-02 20:27:53 +0300

    Recent Updates

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -9,6 +9,11 @@ function NavBar() {
                         <input type="text" placeholder='Add your new task'></input>
                         <button><i className='fas fa-plus'></i></button>
                     </div>
+
+                    <div className='links'>
+                        <a href='/'>Home</a>
+                        <a href='/create'>Create New task</a>
+                    </div>
         </div>
         </div>    
      );
